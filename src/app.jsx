993fbc7097d80e890f7af87de41024733b44a6a8
mykey007 <mmallowe@gmail.com>
2019-05-17 18:59:20 -0400

    updating the date with input and button submit

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,19 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			deadline: 'June 7, 2019'
+			deadline: 'June 7, 2019',
+			newDeadline: '',
 		};
+	}
+
+	changeDeadline() {
+		console.log('state', this.state);
+		this.setState({deadline: this.state.newDeadline});
 	}
 
 	render(){
 		return(
-			<section className="App">
+			<section className="app">
 				<h1>Countdown to {this.state.deadline}</h1>
 				<div className="time">
 					<p>Days: 20</p>
@@ -19,8 +25,12 @@ class App extends Component {
 					<p>Seconds: 20</p>
 				</div>
 				<div>
-					<input placeholder="new date" />
-					<button>Update</button>
+					<input 
+						placeholder="new date" 
+						onChange={event => this.setState({newDeadline: event.target.value})}
+					/>
+					<button onClick={() => this.changeDeadline()}>Update</button>
+					{ /* use anonymous function to prevent loops */ }
 				</div>
 			</section>
 		)
